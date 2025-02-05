88022499ee65b5a8cbfe563d9d01ddfb1336e6c6
KARTHIKREDDY0078 <b.indukarthikreddy@gmail.com>
2025-02-05 12:31:02 +0530

    Add files via upload

---
+++ components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+class NavBar extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>NAvBar</h1>
+            </div>
+        )
+    }
+}
+export default NavBar;
---
+++ components/footer.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+class footer extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>FOOTER</h1>
+            </div>
+        )
+    }
+}
+export default footer;
